ec7a570083d6e41cce3debd05a5e7a3ec5e96fc0
momen124 <momenhossny2004@gmail.com>
2025-01-24 00:37:21 +0200

    fix route error

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,4 +2,9 @@ const express = require('express');
 const router = express.Router();
 const {updateUser} = require('../controllers/userController');
 const authinticate = require('../middleware/authinticate');
+
+
+
 router.patch('/updateUser',authinticate ,updateUser);
+
+module.exports = router;
