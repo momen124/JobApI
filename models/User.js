07ecd23e756a4b2334a9e8609fa8d5d740d1b15c
momen124 <momenhossny2004@gmail.com>
2025-01-24 01:25:33 +0200

    refactor password hashing in user model to skip if not modified

--- models/User.js
+++ models/User.js
@@ -41,9 +41,13 @@ location:{
   },
 })
 
-UserSchema.pre('save', async function () {
+UserSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    return next()
+  }
   const salt = await bcrypt.genSalt(10)
   this.password = await bcrypt.hash(this.password, salt)
+  next()
 })
 
 UserSchema.methods.createJWT = function () {
